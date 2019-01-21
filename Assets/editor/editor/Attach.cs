9649d8b98f174c6eabc31c962efea6c9ef2c7d51
Ashley Rich <46900194+Ashley-Rich@users.noreply.github.com>
2019-01-21 18:40:59 +0000

    Add files via upload

---
+++ Assets/editor/editor/Attach.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Attach : Editor
+{
+    public SimpleAI simpleAI;
+
+    public void AttachScripts()
+    {
+       //foreach(MonoScript mono in simpleAI.behaviourScripts)
+       // {
+       //     simpleAI.aiObject.AddComponent(mono);
+       // }
+    }
+}
---
+++ Assets/editor/editor/SimpleAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SimpleAI : EditorWindow
+{
+    public GameObject aiObject;
+    public MonoBehaviour[] behaviourScripts;
+
+    private bool switchColor;
+
+    [MenuItem("Tools/SimpleAI")]
+    public static void ShowWindow()
+    {
+        GetWindow<SimpleAI>("SimpleAI");
+    }
+
+    public void OnGUI()
+    {
+        aiObject = (GameObject)EditorGUILayout.ObjectField("AI Object", aiObject, typeof(object), true);
+
+        EditorGUILayout.LabelField("AI Behaviour Scripts", EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+
+        ScriptableObject target = this;
+        SerializedObject so = new SerializedObject(target);
+        SerializedProperty StringProperty = so.FindProperty("behaviourScripts");
+
+        EditorGUILayout.PropertyField(StringProperty, true);
+        so.ApplyModifiedProperties();
+
+        EditorGUILayout.Space();
+
+        if(GUILayout.Button("Attach Behaviour Scripts"))
+        {
+
+        }
+
+        EditorGUILayout.Space();
+
+        if(GUILayout.Button("Create AI"))
+        {
+            Instantiate(aiObject, new Vector3(0, 0, 0), Quaternion.identity);
+            aiObject.name = "AI";
+        }
+    }
+}
